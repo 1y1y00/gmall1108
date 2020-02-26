ed3ddfec68597b4592ecacb1f57be96021b5f7ce
杨浩 <501625574@qq.com>
2020-02-26 21:13:32 +0800

    修改readme文件

---
+++ gmall1108-user-manager/src/main/java/com/atguigu/gmall1108/user/Gmall1108UserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall1108.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall1108UserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall1108UserManagerApplication.class, args);
+    }
+
+}
